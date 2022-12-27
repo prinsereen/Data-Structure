17e067f9a9f9b7669f93aa0a5b479a69bba18382
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2022-12-27 14:11:12 +0700

    header created

---
+++ Tubes_Queue.h
@@ -0,0 +1,34 @@
+#ifndef TUBES_QUEUE_H_INCLUDED
+#define TUBES_QUEUE_H_INCLUDED
+#include <iostream>
+#define info(p) p->info
+#define next(p) p->next
+#define first(Q) ((Q).first)
+#define last(Q) ((Q).last)
+
+using namespace std;
+
+struct pasien{
+    string nama;
+    int umur;
+    string tipe;
+    int burst;
+    int waiting;
+};
+
+typedef pasien infotype;
+typedef struct element *adr;
+
+struct element{
+    infotype info;
+    adr next;
+};
+
+struct Queue{
+    adr first;
+    adr last;
+};
+
+
+
+#endif // TUBES_QUEUE_H_INCLUDED
